9e970c11cfb669fe820b11b1228e657bc0e13efb
Kulikov Alexander <shouter84@yandex.ru>
2023-05-29 17:58:16 +0300

    Initial commit, Added new folders Task_041, 043, and solution of Task_041

---
+++ Tasks_041/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41:
+
+// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void Main()
+{
+    int result = NumPositiveCounter(ReadIntArray());
+    System.Console.Write("Number of positive elements in YOUR array: " + result);
+}
+
+int[] ReadIntArray()
+{
+    System.Console.Write("Enter numbers of YOUR array using a spacebar: ");
+    int[] inputArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return inputArray;
+}
+
+int NumPositiveCounter(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) counter++; 
+    }
+    return counter;
+}
+
+Main();
+
